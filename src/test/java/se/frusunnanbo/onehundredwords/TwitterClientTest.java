c2be30de3ec788a35bbd4faf3c0f0ca24722ea26
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-12-12 20:17:23 +0100

    Project setup

---
+++ src/test/java/se/frusunnanbo/onehundredwords/TwitterClientTest.java
@@ -0,0 +1,19 @@
+package se.frusunnanbo.onehundredwords;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by piolin on 12/12/16.
+ */
+public class TwitterClientTest
+{
+    @Test
+    public void true_is_true()
+    {
+        assertThat(true, is(equalTo(true)));
+    }
+}
